e04f1161c916c36bdefef3069f506718b0dcf007
Sroth Sinha <srothsinha@dhcp-39-122-115.dhcpint.csulb.edu>
2024-04-09 00:47:18 -0700

    Push and Commit

---
+++ print("Hello, World!").py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+age=35
+print(age)
+math_operations=+1+3*4/2  #
+print(math_operations)
+float_division=12//3  #integer division no dots in this 
+print(float_division)
+print("Bryan is the gGOAT") #string 
